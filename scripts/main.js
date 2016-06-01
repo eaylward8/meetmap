fe72b176f89f037ed57ac674b91be71549632089
eaylward8 <eaylward8@gmail.com>
2016-06-01 00:18:06 -0400

    Add InfoModal class and info button

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,10 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
 var History = ReactRouter.History;
-// allows for better URLs
-// called in routes variable, inside <Router>
+/* 
+	createBrowserHistory allows for better URLs
+	called in routes variable, inside <Router>
+*/
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var meetupApiAdapter = require('./meetup-api-adapter');
@@ -126,6 +128,7 @@ var App = React.createClass({
 	render: function() {
 		return (
 			<div className="container-fluid">
+				<InfoModal/>
 				<Header/>
 				<div className="row" id="main-container">
 					<div className="col-md-7">
@@ -155,9 +158,43 @@ var Header = React.createClass({
 				  	<div className="navbar-header">
 				  		<img src="../css/images/green-pin.svg" alt="Green Map Pin" id="green-pin"/>
 				  		<span id="meetmap-title">MeetMap</span>
+				  		<button id="info-button" data-toggle="modal" data-target="#myModal">
+				  			<span className="glyphicon glyphicon-info-sign"></span>
+				  		</button>
 				    </div>
 				  </div>
 				</nav>
+			</div>
+		)
+	}
+});
+
+/*
+	InfoModal
+	<InfoModal/>
+*/
+
+var InfoModal = React.createClass({
+	render: function() {
+		return (
+			<div className="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+			  <div className="modal-dialog" role="document">
+			    <div className="modal-content">
+			      <div className="modal-header">
+			        <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			        <h4 className="modal-title" id="myModalLabel">How To Use MeetMap</h4>
+			      </div>
+			      <div className="modal-body">
+			        <p>Enter an address, city, or place name.</p>
+			        <p>Provide a search radius.</p>
+			        <p>MeetMap displays Meetups happening in your area over the next 3 days.</p>
+			        <p>Up to 30 Meetups will be returned, with those scheduled earlier appearing first.</p>
+			      </div>
+			      <div className="modal-footer">
+			        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+			      </div>
+			    </div>
+			  </div>
 			</div>
 		)
 	}
